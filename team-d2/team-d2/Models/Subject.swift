1b6c264283a02539d52c2dabd5630b4c87876f3f
Gaon Kim <gaon.kim.dev@gmail.com>
2019-01-24 01:20:42 +0900

    custom decode 구현

--- team-d2/team-d2/Models/Subject.swift
+++ team-d2/team-d2/Models/Subject.swift
@@ -14,15 +14,13 @@ struct Information: Codable {
     let building: String
 }
 
-struct Subject: Codable {
+struct Subject {
     let academicNumber, classNumber, courseTitle, courseDivision: String
     let domain, department, grade, professorName, score: String
     let information, englishClass, originalClass, onlineClass: String
     let limitPersonal, remark: String
     
-    lazy var informations: [Information] = {
-        return createInformations()
-    }()
+    var informations: [Information] = []
     
     enum CodingKeys: String, CodingKey {
         case academicNumber = "academic_number"
@@ -38,13 +36,35 @@ struct Subject: Codable {
         case limitPersonal = "limit_personal"
         case remark
     }
+}
+
+extension Subject: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        academicNumber = try values.decode(String.self, forKey: .academicNumber)
+        classNumber = try values.decode(String.self, forKey: .classNumber)
+        courseTitle = try values.decode(String.self, forKey: .courseTitle)
+        courseDivision = try values.decode(String.self, forKey: .courseDivision)
+        domain = try values.decode(String.self, forKey: .domain)
+        department = try values.decode(String.self, forKey: .department)
+        grade = try values.decode(String.self, forKey: .grade)
+        professorName = try values.decode(String.self, forKey: .professorName)
+        score = try values.decode(String.self, forKey: .score)
+        information = try values.decode(String.self, forKey: .information)
+        englishClass = try values.decode(String.self, forKey: .englishClass)
+        originalClass = try values.decode(String.self, forKey: .originalClass)
+        onlineClass = try values.decode(String.self, forKey: .onlineClass)
+        limitPersonal = try values.decode(String.self, forKey: .limitPersonal)
+        remark = try values.decode(String.self, forKey: .remark)
+        informations = createInformations()
+    }
     
     private func splitInformation() -> [String] {
         let regex = try? NSRegularExpression(pattern: "\\(.*\\)", options: [])
         let replacedInformation = regex?.stringByReplacingMatches(in: information,
-                                                                options: [],
-                                                                range: NSRange(location: 0, length: information.count),
-                                                                withTemplate: "")
+                                                                  options: [],
+                                                                  range: NSRange(location: 0, length: information.count),
+                                                                  withTemplate: "")
         guard let splittedInformation = replacedInformation?.split(separator: " ").map(String.init) else { return [] }
         
         return splittedInformation
