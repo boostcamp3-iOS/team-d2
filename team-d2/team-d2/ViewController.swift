7dc06833c6200eae2fb7e8bc232bec0652bd7d2c
Tak <tak8997@gmail.com>
2019-01-23 10:21:47 +0900

    initial commit

---
+++ team-d2/team-d2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  team-d2
+//
+//  Created by Tak on 23/01/2019.
+//  Copyright © 2019 boostcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
