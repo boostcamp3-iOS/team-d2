f5c7de133a5e666b0ad9ee8e0103897bc3af0c33
oingbong <oingbong@gmail.com>
2019-01-29 15:32:37 +0900

    PieceLayer 커스텀 레이어 추가

---
+++ DaumWebtoon/Views/PieceLayer.swift
@@ -0,0 +1,35 @@
+//
+//  PieceLayer.swift
+//  DaumWebtoon
+//
+//  Created by oingbong on 29/01/2019.
+//  Copyright © 2019 Gaon Kim. All rights reserved.
+//
+
+import UIKit
+
+class PieceLayer: CAShapeLayer {
+    private let animationKey = "path"
+    
+    // MARK: for StoryBoard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: for Custom
+    init(color: UIColor, path: UIBezierPath) {
+        super.init()
+        self.speed = 0
+        self.timeOffset = 0
+        self.fillColor = color.cgColor
+        self.path = path.cgPath
+    }
+    
+    func configureAnimation(to targetPath: UIBezierPath) {
+        let animation = CABasicAnimation(keyPath: animationKey)
+        animation.duration = 1
+        animation.fromValue = self.path
+        animation.toValue = targetPath.cgPath
+        self.add(animation, forKey: animationKey)
+    }
+}
