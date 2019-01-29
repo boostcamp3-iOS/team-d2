120cdcbc7d465e12fd39804575ea574aaca3a82a
Gaon Kim <gaon.kim.dev@gmail.com>
2019-01-29 13:38:32 +0900

    Revert "스플래쉬 애니메이션 추가"

---
+++ DaumWebtoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaumWebtoon
+//
+//  Created by Gaon Kim on 28/01/2019.
+//  Copyright © 2019 Gaon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
