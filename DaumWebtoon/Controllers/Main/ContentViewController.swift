3cb30cc790bcb3e1a5d1b039286989900029f8a4
Gaon Kim <gaon.kim.dev@gmail.com>
2019-02-13 17:03:34 +0900

    Content VC 데이터 소스 추가

--- DaumWebtoon/Controllers/Main/ContentViewController.swift
+++ DaumWebtoon/Controllers/Main/ContentViewController.swift
@@ -15,13 +15,15 @@ class ContentViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         view.addSubview(tableView)
+        tableView.frame = view.frame
     }
 }
 
 extension ContentViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
