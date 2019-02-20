58a3291cf2d1b3ec76787dc15a059288bf76fb9d
ByungTak <tak8997@gmail.com>
2019-02-20 20:13:51 +0900

    Feature/audio hls (#85)
    * transition에니메이션 시작 위치 버그 수정.
    
    * podcasts뷰컨트롤러 수정중
    
    * 에피소드 뷰에 오디오 관련 리팩토링. PodCast컬렉션뷰 디테일 셀 이름 변경
    
    * 버그 수정 및 화면간 연결
    
    * 오디오 개선, 에피소드 페이지 디자인 수정.
    
    * 에피소드 재생 버튼 이미지 버그 수정.
    
    * 로그 제거
    
    * 오디오 하단 미니플레이어 추가. 오디오 관련 버그 수정 및 리펙토링

---
+++ DaumWebtoon/Controllers/MiniPlayer/MiniPlayerViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MiniPlayerViewController.swift
+//  DaumWebtoon
+//
+//  Created by Tak on 19/02/2019.
+//  Copyright © 2019 Gaon Kim. All rights reserved.
+//
+
+import UIKit
+
+class MiniPlayerViewController: UIViewController {
+
+    @IBOutlet weak var episodeThumbnail: UIImageView!
+    @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var loading: UILabel!
+    
+    var episode: Episode? {
+        didSet {
+            setupViews()
+            setupAndPlayAudio()
+            setupAudioTimer()
+        }
+    }
+    
+    private var audioTimer : Timer?
+    private let audioService = AudioService.shared
+    private var episodeModalViewController: EpisodeModalViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let y = UIScreen.main.bounds.height - (UIApplication.shared.statusBarFrame.size.height + 100)
+        view.frame = CGRect(x: 0, y: y, width: view.superview?.frame.width ?? 0, height: 100)
+    }
+    
+    private func setupViews() {
+        guard let episode = self.episode else { return }
+        
+        episodeTitle.text = episode.title
+        FetchImageService.shared.execute(imageUrl: episode.thumbnail) { [weak self] (image) in
+            guard let self = self else { return }
+            self.episodeThumbnail.image = image
+        }
+        
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(miniPlayerDidTapped(_:))))
+    }
+    
+    private func setupAndPlayAudio() {
+        guard let episode = self.episode else { return }
+        
+        audioService.dataSource = self
+        audioService.setupAndPlayAudio(audioUrl: episode.audio)
+    }
+    
+    private func setupAudioTimer() {
+        audioTimer = Timer(timeInterval: 0.001, target: self, selector: #selector(EpisodeModalViewController.timeInterval), userInfo: nil, repeats: true)
+        RunLoop.current.add(audioTimer!, forMode: RunLoop.Mode.common)
+    }
+    
+    // MARK :- event handling
+    @objc func miniPlayerDidTapped(_ gesture: UITapGestureRecognizer) {
+        guard let episodeModalViewController = UIStoryboard(name: "PodCast", bundle: nil)
+            .instantiateViewController(withIdentifier: "Episode") as? EpisodeModalViewController else { return }
+        episodeModalViewController.episode = episode
+        present(episodeModalViewController, animated: true, completion: nil)
+        
+        self.episodeModalViewController = episodeModalViewController
+    }
+    
+    @IBAction func playPauseDidTapped(_ sender: UIButton) {
+        audioService.togglePlayPause()
+        sender.isSelected = !sender.isSelected
+    }
+    
+    @objc func timeInterval(){
+        audioService.timeInterval()
+    }
+}
+
+extension MiniPlayerViewController: AudioServiceDataSource {
+    func showLoading(alpha: CGFloat) {
+        loading.alpha = alpha
+    }
+}
