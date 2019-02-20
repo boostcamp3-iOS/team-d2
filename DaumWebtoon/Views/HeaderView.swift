a26ab892fb380098a5f1e7120549c024a2d5bc13
oingbong <oingbong@gmail.com>
2019-02-20 13:42:03 +0900

    헤더뷰 이미지 애니메이션 추가

--- DaumWebtoon/Views/HeaderView.swift
+++ DaumWebtoon/Views/HeaderView.swift
@@ -12,9 +12,8 @@ class HeaderView: UIView {
     private var tabTitleLabel = UILabel()
     private var titleLabel = UILabel()
     private var descriptionLabel = UILabel()
-    private var imageView = UIImageView()
     var symbolView = SymbolView(frame: CGRect(origin: CGPoint(x: 20, y: 20), size: CGSize(width: 100, height: 100)))
-    var firstLayer = CALayer()
+    private var headerImageView = HeaderImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,11 +34,11 @@ class HeaderView: UIView {
     
     func configureData(with channel: Channel?, tabContent: TabContent?) {
         configureSymbolView()
+        configureImage()
         guard let tabContent = tabContent else { return }
         configureTabTitle(with: tabContent)
         guard let channel = channel else { return }
         configureTitle(with: channel.title)
-        configureImage(with: channel.image)
         configureDescription(with: channel.description)
     }
     
@@ -67,18 +66,13 @@ class HeaderView: UIView {
         titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
         titleLabel.numberOfLines = 3
     }
-    
-    private func configureImage(with image: String) {
-        addSubview(imageView)
-        FetchImageService.shared.execute(imageUrl: image) {
-            self.imageView.image = $0
-            self.imageView.contentMode = .scaleAspectFit
-        }
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 35).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: symbolView.trailingAnchor, constant: 20).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
+    private func configureImage() {
+        addSubview(headerImageView)
+        headerImageView.translatesAutoresizingMaskIntoConstraints = false
+        headerImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 35).isActive = true
+        headerImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        headerImageView.leadingAnchor.constraint(equalTo: symbolView.trailingAnchor, constant: 20).isActive = true
+        headerImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
     }
     
     private func configureDescription(with description: String) {
@@ -96,39 +90,10 @@ class HeaderView: UIView {
     
     func timeOffset(value: Double) {
         symbolView.timeOffset(value: value)
-        firstLayer.timeOffset = value
+        headerImageView.timeOffset(with: value)
     }
     
     func testAnimation(with headerContentsDictionary: [Int: HeaderContent]) {
-        let genre = MainViewController.Genre.self
-        let firstId = genre.webDesign.rawValue
-        let secondId = genre.programming.rawValue
-        let thirdId = genre.vrAndAr.rawValue
-        let fourthId = genre.startup.rawValue
-        let emptyView = UIView(frame: CGRect(origin: CGPoint(x: 100, y: 100), size: CGSize(width: 100, height: 100)))
-        emptyView.backgroundColor = .white
-        guard let firstImage = headerContentsDictionary[firstId]?.image.cgImage else { return }
-        guard let secondImage = headerContentsDictionary[secondId]?.image.cgImage else { return }
-        guard let thirdImage = headerContentsDictionary[thirdId]?.image.cgImage else { return }
-        guard let fourthImage = headerContentsDictionary[fourthId]?.image.cgImage else { return }
-        
-        firstLayer.contentsGravity = .resizeAspect
-        firstLayer.frame = self.bounds
-        firstLayer.contents = firstImage
-        firstLayer.speed = 0
-        firstLayer.timeOffset = 0
-        
-        let contentAnimation = CAKeyframeAnimation(keyPath: "contents")
-        contentAnimation.duration = 1
-        contentAnimation.isRemovedOnCompletion = false
-        contentAnimation.values = [firstImage, secondImage, thirdImage, fourthImage, firstImage]
-        firstLayer.add(contentAnimation, forKey: "contents")
-        
-        let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
-        opacityAnimation.duration = 1
-        opacityAnimation.isRemovedOnCompletion = false
-        opacityAnimation.values = [1,0,1,0,1,0,1,0,1]
-        firstLayer.add(opacityAnimation, forKey: "opacity")
-        self.layer.addSublayer(firstLayer)
+        headerImageView.configure(with: headerContentsDictionary)
     }
 }
