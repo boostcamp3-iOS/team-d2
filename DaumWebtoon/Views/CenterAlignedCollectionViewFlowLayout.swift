c66e214a3fc5901fd91b5f78ef12592c371c1a82
Tak <tak8997@gmail.com>
2019-02-25 23:43:41 +0900

    주석 제거

--- DaumWebtoon/Views/CenterAlignedCollectionViewFlowLayout.swift
+++ DaumWebtoon/Views/CenterAlignedCollectionViewFlowLayout.swift
@@ -19,23 +19,16 @@ class CenterAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
         let interItemSpacing = minimumInteritemSpacing
         
         var leftMargin: CGFloat = leftPadding
-        var maxY: CGFloat = -1.0 // Modified to determine origin.y for each item
-        var rowSizes: [[CGFloat]] = [] // Tracks the starting and ending x-values for the first and last item in the row
-        var currentRow: Int = 0 // Tracks the current row
+        var maxY: CGFloat = -1.0
+        var rowSizes: [[CGFloat]] = []
+        var currentRow: Int = 0
         attributes.forEach { layoutAttribute in
-            
-            // Each layoutAttribute represents its own item
             if layoutAttribute.frame.origin.y >= maxY {
-                
-                // This layoutAttribute represents the left-most item in the row
                 leftMargin = leftPadding
-                
-                // Register its origin.x in rowSizes for use later
+
                 if rowSizes.count == 0 {
-                    // Add to first row
                     rowSizes = [[leftMargin, 0]]
                 } else {
-                    // Append a new row
                     rowSizes.append([leftMargin, 0])
                     currentRow += 1
                 }
@@ -46,25 +39,17 @@ class CenterAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
             leftMargin += layoutAttribute.frame.width + interItemSpacing
             maxY = max(layoutAttribute.frame.maxY, maxY)
             
-            // Add right-most x value for last item in the row
             rowSizes[currentRow][1] = leftMargin - interItemSpacing
         }
         
-        // At this point, all cells are left aligned
-        // Reset tracking values and add extra left padding to center align entire row
         leftMargin = leftPadding
         maxY = -1.0
         currentRow = 0
         attributes.forEach { layoutAttribute in
-            
-            // Each layoutAttribute is its own item
             if layoutAttribute.frame.origin.y >= maxY {
-                
-                // This layoutAttribute represents the left-most item in the row
                 leftMargin = leftPadding
                 
-                // Need to bump it up by an appended margin
-                let rowWidth = rowSizes[currentRow][1] - rowSizes[currentRow][0] // last.x - first.x
+                let rowWidth = rowSizes[currentRow][1] - rowSizes[currentRow][0]
                 let appendedMargin = (collectionView!.frame.width - leftPadding  - rowWidth - leftPadding) / 2
                 leftMargin += appendedMargin
                 
